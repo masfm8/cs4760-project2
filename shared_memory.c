2a30fbe20493ef897fb940efb2e889f730251785
MD SARKER <131039792+masfm8@users.noreply.github.com>
2024-09-12 00:40:05 -0500

    Update shared_memory.c

--- shared_memory.c
+++ shared_memory.c
@@ -4,42 +4,26 @@
 #include <sys/shm.h>
 #include "shared_memory.h"
 
-#define SHM_KEY 0x12345
-#define SHM_SIZE sizeof(int) * 2  // Two integers for seconds and nanoseconds
+int shm_id;
+key_t key = 859047; // Shared memory key
 
-static int shm_id;
-static int *system_clock;
-
-void init_shared_memory() {
-    shm_id = shmget(SHM_KEY, SHM_SIZE, IPC_CREAT | 0666);
-    if (shm_id < 0) {
-        perror("shmget");
+void setup_shared_memory() {
+    // Create shared memory for clock
+    shm_id = shmget(key, sizeof(int) * 2, IPC_CREAT | 0666);
+    if (shm_id == -1) {
+        perror("shmget failed");
         exit(1);
     }
-    system_clock = (int *)shmat(shm_id, NULL, 0);
-    if (system_clock == (int *) -1) {
-        perror("shmat");
-        exit(1);
-    }
-    system_clock[0] = 0;  // Seconds
-    system_clock[1] = 0;  // Nanoseconds
 }
 
-int *attach_shared_memory() {
-    int *shm_ptr = (int *) shmat(shm_id, NULL, 0);
-    if (shm_ptr == (int *) -1) {
-        perror("shmat");
-        exit(1);
-    }
-    return shm_ptr;
+void attach_shared_memory(int **shared_clock_sec, int **shared_clock_ns) {
+    // Attach to shared memory
+    int *shm_ptr = (int *)shmat(shm_id, NULL, 0);
+    *shared_clock_sec = shm_ptr;
+    *shared_clock_ns = shm_ptr + 1;
 }
 
 void detach_shared_memory() {
-    if (shmdt(system_clock) == -1) {
-        perror("shmdt");
-    }
-}
-
-void cleanup_shared_memory() {
-    shmctl(shm_id, IPC_RMID, NULL);
+    shmdt(shared_clock_sec);
+    shmdt(shared_clock_ns);
 }
