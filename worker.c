26ec394513c1ddab3c21e51287c18df846158875
MD SARKER <131039792+masfm8@users.noreply.github.com>
2024-09-26 00:04:27 -0500

    Update worker.c

--- worker.c
+++ worker.c
@@ -1,30 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
-#include "shared_memory.h"
+#include <signal.h>
+
+#define SHMKEY 9876
+
+int shm_id;
+int *sharedClock;
+
+void cleanupOnSignal(int sig) {
+    if (sig == SIGINT) {
+        shmdt(sharedClock);
+        exit(0);
+    }
+}
 
 int main(int argc, char *argv[]) {
-    // Attach to shared memory for system clock
-    int *shared_clock_sec, *shared_clock_ns;
-    attach_shared_memory(&shared_clock_sec, &shared_clock_ns);
-    
-    // Fetch max time to stay in the system (from command line)
-    int max_sec = atoi(argv[1]);
-    int max_ns = atoi(argv[2]);
+    signal(SIGINT, cleanupOnSignal);
 
-    // Calculate termination time based on shared clock
-    int term_sec = *shared_clock_sec + max_sec;
-    int term_ns = *shared_clock_ns + max_ns;
+    if (argc < 2) {
+        fprintf(stderr, "worker: Missing argument for time to stay.\n");
+        exit(1);
+    }
+
+    shm_id = shmget(SHMKEY, 2 * sizeof(int), 0666);
+    if (shm_id == -1) {
+        perror("worker: shmget");
+        exit(1);
+    }
+
+    sharedClock = (int *)shmat(shm_id, NULL, 0);
+    if (sharedClock == (int *)-1) {
+        perror("worker: shmat");
+        exit(1);
+    }
+
+    int staySecs = atoi(argv[1]);
+    int stayNano = rand() % 1000000000;
+
+    int terminateSec = sharedClock[0] + staySecs;
+    int terminateNano = sharedClock[1] + stayNano;
+    if (terminateNano >= 1000000000) {
+        terminateSec += 1;
+        terminateNano -= 1000000000;
+    }
+
+    printf("WORKER PID: %d PPID: %d StartS: %ds StartN: %dns TermTimeS: %ds TermTimeN: %dns Starting\n",
+           getpid(), getppid(), sharedClock[0], sharedClock[1], terminateSec, terminateNano);
 
     while (1) {
-        if (*shared_clock_sec >= term_sec && *shared_clock_ns >= term_ns) {
-            printf("Worker terminating\n");
+        if (sharedClock[0] > terminateSec || (sharedClock[0] == terminateSec && sharedClock[1] >= terminateNano)) {
+            printf("WORKER PID: %d PPID: %d EndS: %ds EndN: %dns TermTimeS: %ds TermTimeN: %dns Terminating\n",
+                   getpid(), getppid(), sharedClock[0], sharedClock[1], terminateSec, terminateNano);
             break;
         }
     }
 
+    shmdt(sharedClock);
     return 0;
 }
