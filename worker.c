89d399d430f05c166f967631edae1240d01fe167
MD SARKER <131039792+masfm8@users.noreply.github.com>
2024-12-15 20:51:23 -0600

    Update worker.c

--- worker.c
+++ worker.c
@@ -4,60 +4,54 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <signal.h>
+#include <stdbool.h>
 
 #define SHMKEY 9876
 
-int shm_id;
-int *sharedClock;
+int shmid;
+int *shmClock;
 
-void cleanupOnSignal(int sig) {
-    if (sig == SIGINT) {
-        shmdt(sharedClock);
+// Signal handler for SIGINT
+void sig_handler(int signo) {
+    if (signo == SIGINT) {
+        shmdt(shmClock);
         exit(0);
     }
 }
 
 int main(int argc, char *argv[]) {
-    signal(SIGINT, cleanupOnSignal);
+    signal(SIGINT, sig_handler); // Handle SIGINT to clean up
 
     if (argc < 2) {
-        fprintf(stderr, "worker: Missing argument for time to stay.\n");
+        fprintf(stderr, "WORKER: Missing time to stay argument.\n");
         exit(1);
     }
 
-    shm_id = shmget(SHMKEY, 2 * sizeof(int), 0666);
-    if (shm_id == -1) {
-        perror("worker: shmget");
+    int retries = 5;
+    while (retries--) {
+        shmid = shmget(SHMKEY, 2 * sizeof(int), 0666);
+        if (shmid != -1) break;
+        usleep(500000); // Retry delay
+    }
+    if (shmid == -1) {
+        fprintf(stderr, "WORKER: Failed to get shared memory.\n");
         exit(1);
     }
 
-    sharedClock = (int *)shmat(shm_id, NULL, 0);
-    if (sharedClock == (int *)-1) {
-        perror("worker: shmat");
+    shmClock = (int *)shmat(shmid, NULL, 0);
+    if (shmClock == (int *)-1) {
+        fprintf(stderr, "WORKER: Failed to attach to shared memory.\n");
         exit(1);
     }
 
-    int staySecs = atoi(argv[1]);
-    int stayNano = rand() % 1000000000;
+    int secondsToStay = atoi(argv[1]);
+    printf("WORKER PID: %d attached to shared memory.\n", getpid());
 
-    int terminateSec = sharedClock[0] + staySecs;
-    int terminateNano = sharedClock[1] + stayNano;
-    if (terminateNano >= 1000000000) {
-        terminateSec += 1;
-        terminateNano -= 1000000000;
+    while (shmClock[0] < secondsToStay) {
+        usleep(1000);
     }
 
-    printf("WORKER PID: %d PPID: %d StartS: %ds StartN: %dns TermTimeS: %ds TermTimeN: %dns Starting\n",
-           getpid(), getppid(), sharedClock[0], sharedClock[1], terminateSec, terminateNano);
-
-    while (1) {
-        if (sharedClock[0] > terminateSec || (sharedClock[0] == terminateSec && sharedClock[1] >= terminateNano)) {
-            printf("WORKER PID: %d PPID: %d EndS: %ds EndN: %dns TermTimeS: %ds TermTimeN: %dns Terminating\n",
-                   getpid(), getppid(), sharedClock[0], sharedClock[1], terminateSec, terminateNano);
-            break;
-        }
-    }
-
-    shmdt(sharedClock);
+    shmdt(shmClock);
+    printf("WORKER PID: %d exiting.\n", getpid());
     return 0;
 }
